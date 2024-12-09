4dd075587236c67234894de5e2bc9d2cde5a4fc1
Cooper-mastin8 <cooper.mastin8@gmail.com>
2024-12-08 20:02:52 -0500

    Cleaned dataset and imported umap

--- FinalProject.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-data_path = "BME3053C_FinalProject/DASH_-_Global_School-based_Student_Health_Survey__GSHS.csv"
-data = pd.read_csv(data_path)
-key_columns = ["Year","LocationDesc","QuestionCode","Greater_Risk_Data_Value","Sex","Age"]
-data = data[key_columns]
-
-print(data.head(5))
